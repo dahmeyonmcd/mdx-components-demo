fe880313a173a18be5f36dc76dc5f2b6289e7325
Dahmeyon McDonald <40929982+dahmeyonmcd@users.noreply.github.com>
2025-02-18 20:47:06 -0500

    Updated

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
+"use client"
+
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
-import localFont from "@next/font/local";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -12,13 +13,10 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-const darkForest = localFont({ src: '../../public/fonts/DarkForest/DarkForestBold.ttf'})
-const pixiDot = localFont({ src: '../../public/fonts/DarkForest/DarkForestBold.ttf'})
-
 export default function Home() {
   return (
     <div
-      className={`${geistSans.variable} ${geistMono.variable} ${darkForest?.className} ${pixiDot.className} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
+      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
     >
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
